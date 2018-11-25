6b32aed32d1430bf21207b10ed46aeb0f53564fc
Acaua Sperl de Faria <acaua.faria@gmail.com>
2018-11-25 02:34:37 -0200

    Hello react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello React</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Hello React</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
