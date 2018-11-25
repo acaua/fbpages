827314e2bab776b38974184b66b06cd1d9189a07
Acaua Sperl de Faria <acaua.faria@gmail.com>
2018-11-25 02:03:40 -0200

    Serve static react client build folder

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const pages = require("./routes/pages");
 const app = express();
 app.use("/api/", pages);
 
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
